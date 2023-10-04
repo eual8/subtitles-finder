30c41d962635164df58bb7a568b0e991c5a637fe
eugen <aleksandrov.eugen@gmail.com>
2023-10-04 13:09:41 +0300

    Add playlist_id to ES index

--- resources/views/filament/pages/search.blade.php
+++ resources/views/filament/pages/search.blade.php
@@ -9,7 +9,7 @@
         </button>
 
         <select wire:model="videoId" wire:change="search" class="mt-2 w-1/2">
-            <option value=0>Select video</option>
+            <option value=0>All videos</option>
             @foreach($videos as $key => $video)
                 <option value="{{ $key }}">{{ $video }}</option>
             @endforeach
