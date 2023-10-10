9e67fee3cfc7b60cf5288048493ed88ba06cf323
eugen <aleksandrov.eugen@gmail.com>
2023-10-10 16:49:19 +0300

    Fix Gate is User have Admin role check

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
@@ -20,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::before(function ($user, $ability) {
+        Gate::after(function (User $user, string $ability, ?bool $result, mixed $arguments) {
             return $user->hasRole('admin');
         });
     }
