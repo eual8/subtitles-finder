4a49a668a260ef0711025bb03d548ade35afe3c3
eugen <aleksandrov.eugen@gmail.com>
2023-10-11 12:42:31 +0300

    Add show all Playlists endpoint, add show one playlist endpoint

---
+++ app/Http/Controllers/Api/V1/PlaylistController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\V1\PlaylistCollection;
+use App\Http\Resources\V1\PlaylistResource;
+use App\Models\Playlist;
+use Illuminate\Http\Request;
+
+class PlaylistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return new PlaylistCollection(Playlist::all());
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Playlist $playlist)
+    {
+        return new PlaylistResource($playlist);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Playlist $playlist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Playlist $playlist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Playlist $playlist)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\AuthController;
+use App\Http\Controllers\Api\V1\PlaylistController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -24,5 +25,7 @@ Route::prefix('v1')->group(static function () {
         Route::get('/user', function (Request $request) {
             return $request->user();
         });
+
+        Route::resource('playlists', PlaylistController::class);
     });
 });
