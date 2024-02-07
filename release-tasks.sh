dfe0f8bf0816f6fe4d5dba547f2d23a2e5a358e8
eugen <aleksandrov.eugen@gmail.com>
2024-02-07 17:26:53 +0200

    Fix typo in release file.

--- release-tasks.sh
+++ release-tasks.sh
@@ -3,7 +3,7 @@
 ## More info in Heroku doc page https://devcenter.heroku.com/articles/release-phase#design-considerations
 
 ## Steps to execute
-php artisan:migrate --force
+php artisan migrate --force
 php artisan storage:link
 php artisan cache:clear
 php artisan config:clear
