5ff89b6a44b3f4d672f06aa292620c7e69d8452d
eugen <aleksandrov.eugen@gmail.com>
2024-02-07 17:21:06 +0200

    Update Procfile - add release commands

---
+++ release-tasks.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+## More info in Heroku doc page https://devcenter.heroku.com/articles/release-phase#design-considerations
+
+## Steps to execute
+php artisan:migrate --force
+php artisan storage:link
+php artisan cache:clear
+php artisan config:clear
+
+npm i
+npm run build
+
+# check for a good exit
+if [ $? -ne 0 ]
+then
+  # something went wrong; convey that and exit
+  exit 1
+fi
