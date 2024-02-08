f6210ec9f20a72720510586ff005dec81d3c72f1
eugen <aleksandrov.eugen@gmail.com>
2024-02-08 18:50:32 +0200

    forceScheme to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
+
         Gate::after(function (User $user, string $ability, ?bool $result, mixed $arguments) {
             return $user->hasRole('admin');
         });
