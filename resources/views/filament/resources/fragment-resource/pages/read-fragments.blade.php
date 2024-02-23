421b2cdf3cf19ecb3b0917a2b38ecc62569a8acd
eugen <aleksandrov.eugen@gmail.com>
2024-02-23 17:40:54 +0200

    replace p tag to span + br

--- resources/views/filament/resources/fragment-resource/pages/read-fragments.blade.php
+++ resources/views/filament/resources/fragment-resource/pages/read-fragments.blade.php
@@ -2,7 +2,7 @@
     @include('filament.read_buttons')
 
     @foreach($fragments as $fragment)
-        <p>{{ $fragment->text }}</p>
+        <span>{{ $fragment->text }}</span><br>
     @endforeach
 
     @include('filament.read_buttons')
