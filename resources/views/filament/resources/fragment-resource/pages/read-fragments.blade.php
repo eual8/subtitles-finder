4d077cf905da8bfa1d6c4edbc29eb93ccd5ce85c
eugen <aleksandrov.eugen@gmail.com>
2024-02-23 17:44:22 +0200

    update p tag in fragments

--- resources/views/filament/resources/fragment-resource/pages/read-fragments.blade.php
+++ resources/views/filament/resources/fragment-resource/pages/read-fragments.blade.php
@@ -1,9 +1,7 @@
 <x-filament-panels::page>
     @include('filament.read_buttons')
 
-    @foreach($fragments as $fragment)
-        <span>{{ $fragment->text }}</span><br>
-    @endforeach
+    @foreach($fragments as $fragment)<p title="{{ $fragment->time_string }}">{{ $fragment->text }}</p>@endforeach
 
     @include('filament.read_buttons')
 </x-filament-panels::page>
