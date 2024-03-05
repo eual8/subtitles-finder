30b8d807344e2c9ebc2d127d8d93aed9006b6acc
eugen <aleksandrov.eugen@gmail.com>
2024-03-05 18:39:26 +0200

    Fix search link to read fragment

--- resources/views/filament/pages/search.blade.php
+++ resources/views/filament/pages/search.blade.php
@@ -34,10 +34,10 @@
 
     @foreach($fragments->hits() as $hit)
 
-        <a href="/admin/fragments/{{ $hit->model()->id }}/read" target="_blank">
+        <a href="/admin/fragments/{{ $hit->model()?->id }}/read" target="_blank">
             <div class="flex items-center border-b border-gray-300 py-2">
                 <div class="block">
-                    <img src="{{ $hit->model()->video_image }}" title="{{ $hit->model()->video->title }}"
+                    <img src="{{ $hit->model()?->video_image }}" title="{{ $hit->model()?->video->title }}"
                          style="max-width: 120px" class="object-cover block p-2">
                 </div>
 
